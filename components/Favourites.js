fe6c3dc1b9fc2d87c02f4e44b3e7c7ce380b4a39
aaron_tetracube <aaronpsam@gmail.com>
2021-02-22 11:02:09 +0530

    Favourites page features added.

--- components/Favourites.js
+++ components/Favourites.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, Text, View } from "react-native";
+import { Image, Text, View,StyleSheet } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
 class Favourites extends Component{
@@ -8,15 +8,37 @@ class Favourites extends Component{
     }
     render(){
         return(
-            <View>
-                <Text>This is Favourites.</Text>
-                <Text>what</Text>
-                <TouchableOpacity onPress ={() => console.log("Clicked")}>
-                    <Text>Button</Text>
-                </TouchableOpacity>
-            </View>
-            
+            <View style={styles.container}>                       
+                <Text style={{fontSize:20, marginTop:10,marginLeft:20, marginBottom:35, textAlign:"center"}}><Text style={{fontWeight:"bold"}}>Favourites</Text></Text>
+                <Text style={{fontSize:15,marginLeft:20, marginBottom:35, textAlign:"center"}}>You are in Favourites section.</Text>
+            </View>   
         );
     }
 }
+const styles = StyleSheet.create({
+   
+    container:{
+        width:"100%",
+        display:"flex",
+        flex:1,
+        flexDirection:"column",
+        justifyContent:"center",
+        paddingTop:10,
+        //backgroundColor:"red",
+    },
+    logbut:{
+        borderRadius: 10,
+        marginTop:10,
+        marginBottom:20,
+        marginLeft:20,
+        marginRight:20,
+        backgroundColor:"blue",
+        padding:10,
+    },
+    voidspace:{
+        width:"100%",
+        height:130,
+        //backgroundColor:"yellow",
+    },
+});
 export default Favourites;

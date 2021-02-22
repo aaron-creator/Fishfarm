4dae8f7408d1da540f98756cd708ed271c3283a5
aaron_tetracube <aaronpsam@gmail.com>
2021-02-22 11:04:26 +0530

    Cart setion features added.

--- components/Cart.js
+++ components/Cart.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
 class Cart extends Component{
@@ -8,15 +8,37 @@ class Cart extends Component{
     }
     render(){
         return(
-            <View>
-                <Text>This is Cart.</Text>
-                <Text>what</Text>
-                <TouchableOpacity onPress ={() => console.log("Clicked")}>
-                    <Text>Button</Text>
-                </TouchableOpacity>
+            <View style={styles.container}>                       
+                <Text style={{fontSize:20, marginTop:10,marginLeft:20, marginBottom:35, textAlign:"center"}}><Text style={{fontWeight:"bold"}}>Cart</Text></Text>
+                <Text style={{fontSize:15,marginLeft:20, marginBottom:35, textAlign:"center"}}>You are in Cart section.</Text>
             </View>
-            
         );
     }
 }
+const styles = StyleSheet.create({
+   
+    container:{
+        width:"100%",
+        display:"flex",
+        flex:1,
+        flexDirection:"column",
+        justifyContent:"center",
+        paddingTop:10,
+        //backgroundColor:"red",
+    },
+    logbut:{
+        borderRadius: 10,
+        marginTop:10,
+        marginBottom:20,
+        marginLeft:20,
+        marginRight:20,
+        backgroundColor:"blue",
+        padding:10,
+    },
+    voidspace:{
+        width:"100%",
+        height:130,
+        //backgroundColor:"yellow",
+    },
+});
 export default Cart;
